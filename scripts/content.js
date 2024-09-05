7054639c1293f2be78025f7e661149545f27ac4c
rburakevich <rburakevich@tecel.ru>
2024-09-05 20:44:45 +0300

    [ref] core

--- scripts/content.js
+++ scripts/content.js
@@ -21,26 +21,77 @@ const siteDetect = () => {
   return null
 }
 
-const globalParser = (param) => {
-  const reviews = document.querySelectorAll(`${param.review_wrap_selector}:not([parsed=true])`);
+const site = siteDetect();
+const param = paramSet[site]
 
-  for (const review of reviews) {
-    const uid = (Date.now() + Math.random()).toString(32);
-    review.setAttribute('parsed', 'true');
-    review.setAttribute('data-uid', uid);
-    const div = document.createElement('div');
-    div.setAttribute('class', 'badge');
-    div.innerText = `parsed: ${uid}`
-    const context = review.querySelector(param.review_text_selector);
+const placeBadge = ({analysis}) => {
 
-    console.log( param.parseFn(context) );
 
-    review.appendChild(div);
-  }
+  // analysis.forEach((i) => {
+  //   const div = document.createElement('div');
+  //   div.setAttribute('class', 'badge');
+  //   div.innerText = `parsed: ${i.uid}`
+  //   const review = document.querySelector(`${param.review_wrap_selector} [parsed=true]`);
+  //   review.appendChild(div)
+  // })
 }
 
-const site = siteDetect();
+const fetchApi = (data) => {
+  // fake api request
+  new Promise((resolve) => {
+    setTimeout(() => {
+      const res = {
+        analysis: data.map((i) => {
+          return {
+            uid: i.uid,
+            origin: i.comment,
+            sentiment: "positive",
+            confidence: 0.95
+          }
+        })
+      }
+
+      console.log('response', res)
+
+      resolve(res)
+    }, 1000)
+  }).then(placeBadge);
+
+
+  // todo:
+  //  fetch('url', {
+  //    method: 'POST',
+  //    body: JSON.stringify(data),
+  //  })
+  //   .then(res => res.json())
+  //   .then(placeBadge)
+}
+
+const globalParser = () => {
+  const reviews = document.querySelectorAll(`${param.review_wrap_selector}:not([parsed=true])`);
+  if (!reviews || reviews.length === 0) return;
+
+  let dataArray = [];
+
+  for (const review of reviews) {
+    const uid = (Date.now().toString(16) + Math.random().toString(16).slice(2));
+    review.classList.add('analyze');
+    review.setAttribute('parsed', 'true');
+    review.setAttribute('data-uid', uid);
+
+    const context = review.querySelector(param.review_text_selector);
+
+    dataArray.push({
+      uid: uid,
+      comment: param.parseFn(context)
+    })
+  }
+
+  console.log('request', dataArray)
+
+  fetchApi(dataArray);
+}
 
 if (site) {
-  window.addEventListener('scroll', () => globalParser( paramSet[site]));
+  window.addEventListener('scroll', () => globalParser());
 }
