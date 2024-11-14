0916b45a97df3175be9143e1e214bd621be90e86
rburakevich <rburakevich@tecel.ru>
2024-11-14 17:50:16 +0300

    [+] api

--- scripts/background.js
+++ scripts/background.js
@@ -1,48 +1,54 @@
 const dataSet = {}
 
 const countTotal = (dataSet) => {
-  const positive = dataSet?.countPositive ?? 0;
-  const neutral = dataSet?.countNeutral ?? 0;
-  const negative = dataSet?.countNegative ?? 0;
+    const positive = dataSet?.countPositive ?? 0;
+    const neutral = dataSet?.countNeutral ?? 0;
+    const negative = dataSet?.countNegative ?? 0;
 
-  return (positive + negative + neutral).toString();
+    return (positive + negative + neutral).toString();
 }
 
 const enablePopup = (tabId) => {
-  chrome.action.setPopup({ tabId, popup: 'popup.html' }).then();
-  chrome.action.setBadgeText({text: countTotal(dataSet[tabId])}).then();
+    chrome.action.setPopup({tabId, popup: 'popup.html'}).then();
+    chrome.action.setBadgeText({text: countTotal(dataSet[tabId])}).then();
 }
 
 const disablePopup = () => {
-  chrome.action.setBadgeText({text: "OFF"}).then();
-  chrome.action.setPopup({ popup: 'disable.html' }).then();
+    chrome.action.setBadgeText({text: "OFF"}).then();
+    chrome.action.setPopup({popup: 'disable.html'}).then();
 }
 
 const prepareDataset = (tabId, msg) => {
-  Object.assign(dataSet[tabId], msg?.info ?? {});
-  chrome.action.setBadgeText({text: countTotal(dataSet[tabId])}).then();
+    if (!dataSet[tabId]) return;
+    Object.assign(dataSet[tabId], msg?.info ?? {});
+    chrome.action.setBadgeText({text: countTotal(dataSet[tabId])}).then();
 }
 
 chrome.tabs.onActivated.addListener((activeInfo) => {
-  if (!dataSet[activeInfo.tabId]) return disablePopup();
-  enablePopup(activeInfo.tabId);
+    if (!dataSet[activeInfo.tabId]) return disablePopup();
+    enablePopup(activeInfo.tabId);
 });
 
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.from === 'popup') {
-    switch (msg.subject) {
-      case 'get_data': return sendResponse(dataSet[msg.tabId]);
+    if (msg.from === 'popup') {
+        switch (msg.subject) {
+            case 'get_data':
+                return sendResponse(dataSet[msg.tabId]);
+        }
+        return;
     }
-    return ;
-  }
 
-  if (msg.from === 'content') {
-    switch (msg.subject) {
-      case 'init': return dataSet[sender.tab.id] = {};
-      case 'enable': return enablePopup(sender.tab.id);
-      case 'disable': return disablePopup();
-      case 'dataset': return prepareDataset(sender.tab.id, msg)
+    if (msg.from === 'content') {
+        switch (msg.subject) {
+            case 'init':
+                return dataSet[sender.tab.id] = {};
+            case 'enable':
+                return enablePopup(sender.tab.id);
+            case 'disable':
+                return disablePopup();
+            case 'dataset':
+                return prepareDataset(sender.tab.id, msg)
+        }
     }
-  }
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,34 +1,39 @@
 const paramSet = {
-  market: {
-    review_wrap_selector: '[data-auto="review-item"]',
-    parseFn: (ctx) => {
-      const el = ctx.querySelector('[itemprop="description"]');
-      return el?.getAttribute('content')
+    market: {
+        review_wrap_selector: '[data-auto="review-item"]',
+        parseFn: (ctx) => {
+            const el = ctx.querySelector('script');
+            try {
+                const json = JSON.parse(el?.textContent)
+                return json.review.reviewBody;
+            } catch {
+                return '';
+            }
+        },
     },
-  },
 
-  ozon: {
-    review_wrap_selector: '[data-review-uuid]',
-    parseFn: (ctx) => {
-      const el = ctx.children[1]?.children[1];
-      return el?.querySelector('span')?.textContent.trim();
+    ozon: {
+        review_wrap_selector: '[data-review-uuid]',
+        parseFn: (ctx) => {
+            const el = ctx.children[1]?.children[1];
+            return el?.querySelector('span')?.textContent.trim();
+        },
     },
-  },
 
-  wildberries: {
-    review_wrap_selector: '[itemprop="review"]',
-    parseFn: (ctx) => {
-      const el = ctx.querySelector('[itemprop="reviewBody"]');
-      return el?.textContent.trim();
-    },
-  }
+    wildberries: {
+        review_wrap_selector: '[itemprop="review"]',
+        parseFn: (ctx) => {
+            const el = ctx.querySelector('[itemprop="reviewBody"]');
+            return el?.textContent.trim();
+        },
+    }
 }
 
 const siteDetect = (native) => {
-  if (location.host.includes('market')) return native ? 'Яндекс маркет' : 'market';
-  if (location.host.includes('ozon')) return native ? 'OZON' : 'ozon';
-  if (location.host.includes('wildberries')) return native ? 'Wildberries' : 'wildberries';
-  return null;
+    if (location.host.includes('market')) return native ? 'Яндекс маркет' : 'market';
+    if (location.host.includes('ozon')) return native ? 'OZON' : 'ozon';
+    if (location.host.includes('wildberries')) return native ? 'Wildberries' : 'wildberries';
+    return null;
 }
 
 const site = siteDetect();
@@ -39,135 +44,121 @@ let countNegative = 0;
 let countNeutral = 0;
 let confidence = 0;
 
-const placeBadge = ({analysis}) => {
-  analysis.forEach((i) => {
+const sentimentPrepare = (sent) => {
+    switch (sent) {
+        case 'positive':
+            countPositive++;
+            return {title: 'Полезный отзыв', icon: '🔥'};
+
+        case 'negative':
+            countNegative++;
+            return {title: 'Бесполезный отзыв', icon: '😶'};
+
+        case 'neutral':
+            countNeutral++;
+            return {title: 'Нейтральный отзыв', icon: '💩'};
+    }
+}
+
+const placeBadge = (i) => {
     const div = document.createElement('div');
     div.classList.add('badge');
     div.classList.add(i.sentiment);
 
-    const sentimentPrepare = (sent) => {
-      switch (sent) {
-        case 'positive':
-          countPositive++;
-          return {title: 'Полезный отзыв', icon: '🔥'};
-        case 'negative':
-          countNegative++;
-          return {title: 'Бесполезный отзыв', icon: '😶'};
-        case 'neutral':
-          countNeutral++;
-          return {title: 'Нейтральный отзыв', icon: '💩'};
-      }
-    }
-
     confidence += +i.confidence
 
-    let innerHtml = `<span class="smile" title="${sentimentPrepare(i.sentiment).title}">${sentimentPrepare(i.sentiment).icon}</span><span class="confidence" title="Степень достоверности нашей оценки">${i.confidence}</span>`;
-    if (i.robot) innerHtml = `<span class="robot" title="Похоже отзыв написан не человеком или ради накрутки">${i.robot}</span>` + innerHtml;
+    const sentiment = sentimentPrepare(i.sentiment);
+
+    let innerHtml = `<span class="smile" title="${sentiment.title}">${sentiment.icon}</span><span class="confidence" title="Степень достоверности нашей оценки">${i.confidence}</span>`;
     div.innerHTML = innerHtml;
     const review = document.querySelector(`[data-uid="${i.uid}"]`);
+    review.classList.add(i.sentiment);
+
     review.appendChild(div);
 
 
+    console.log({countPositive, countNegative, countNeutral, confidence})
+
     chrome.runtime.sendMessage({
-      from: 'content',
-      subject: 'dataset',
-      info: {countPositive, countNegative, countNeutral, confidence}
+        from: 'content',
+        subject: 'dataset',
+        info: {countPositive, countNegative, countNeutral, confidence}
     }).then();
-  })
 }
 
-const fetchApi = (data) => {
-  // fake api request
-  new Promise((resolve) => {
-    const sentiments = ['positive', 'negative', 'neutral'];
-
-    setTimeout(() => {
-      const res = {
-        analysis: data.map((i) => {
-          return {
-            uid: i.uid,
-            origin: i.comment,
-            sentiment: sentiments[Math.floor(Math.random() * sentiments.length)],
-            confidence: Math.random().toFixed(2),
-            robot: Math.random() < 0.5 ? '🤖' : ''
-          }
+const fetchApi = (dataArr) => {
+    dataArr.forEach(data => {
+        const {uid, comment} = data;
+        console.log(data)
+        fetch('http://127.0.0.1:8000/analyze_review', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({'uid': uid, 'comment': comment}),
         })
-      }
-
-      console.log('response', res)
-
-      resolve(res)
-    }, 1000)
-  }).then(placeBadge);
-
-
-  // todo:
-  //  fetch('url', {
-  //    method: 'POST',
-  //    body: JSON.stringify(data),
-  //  })
-  //   .then(res => res.json())
-  //   .then(placeBadge)
+            .then(res => res.json())
+            .then(placeBadge)
+    })
 }
 
 const globalParser = () => {
-  const reviews = document.querySelectorAll(`${param.review_wrap_selector}:not([parsed=true])`);
-  if (!reviews.length) return;
+    const reviews = document.querySelectorAll(`${param.review_wrap_selector}:not([parsed=true])`);
+    if (!reviews.length) return;
 
-  let dataArray = [];
+    let dataArray = [];
 
-  for (const review of reviews) {
-    const uid = (Date.now().toString(16) + Math.random().toString(16).slice(2));
-    review.classList.add('analyze');
-    review.setAttribute('parsed', 'true');
-    review.setAttribute('data-uid', uid);
+    for (const review of reviews) {
+        const uid = (Date.now().toString(16) + Math.random().toString(16).slice(2));
+        review.classList.add('analyze');
+        review.setAttribute('parsed', 'true');
+        review.setAttribute('data-uid', uid);
 
-    dataArray.push({
-      uid: uid,
-      comment: param.parseFn(review)
-    })
-  }
+        dataArray.push({
+            uid: uid,
+            comment: param.parseFn(review)
+        })
+    }
 
-  console.log('request', dataArray)
+    console.log('request', dataArray)
 
-  fetchApi(dataArray);
+    fetchApi(dataArray);
 }
 
 chrome.runtime.sendMessage({
-  from: 'content',
-  subject: site ? 'enable' : 'disable',
+    from: 'content',
+    subject: site ? 'enable' : 'disable',
 }).then();
 
 chrome.runtime.onMessage.addListener((msg, sender) => {
-  console.log(msg, sender)
-  if (msg.from === 'popup') {
-    switch (msg.subject) {
-      case 'toggle_visible':
-        const badges = document.querySelectorAll('.analyze > .badge');
+    console.log(msg, sender)
+    if (msg.from === 'popup') {
+        switch (msg.subject) {
+            case 'toggle_visible':
+                return document.querySelectorAll('.analyze .badge').forEach(el => {
+                    msg.value ? el.classList.add('hide') : el.classList.remove('hide');
+                });
 
-        console.log(badges)
-
-        document.querySelectorAll('.analyze .badge').forEach(el => {
-          msg.value ? el.classList.add('hide') : el.classList.remove('hide');
-        });
-        break;
+            case 'toggle_positive':
+            case 'toggle_negative':
+            case 'toggle_neutral':
+        }
     }
-  }
 });
 
 if (site) {
-  chrome.runtime.sendMessage({
-    from: 'content',
-    subject: 'init',
-  }).then();
+    chrome.runtime.sendMessage({
+        from: 'content',
+        subject: 'init',
+    }).then();
 
-  chrome.runtime.sendMessage({
-    from: 'content',
-    subject: 'dataset',
-    info: { market: siteDetect(true) }
-  }).then();
+    chrome.runtime.sendMessage({
+        from: 'content',
+        subject: 'dataset',
+        info: {market: siteDetect(true)}
+    }).then();
 
-  window.addEventListener('scroll', () => globalParser());
+    window.addEventListener('scroll', () => globalParser());
 }
 
 
